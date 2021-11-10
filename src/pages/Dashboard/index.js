7b8d0597c21497839a21b07ef09ee3ef342f8eba
Wesley <wesley.brazil@outlook.com>
2021-11-09 22:05:24 -0300

    Develop: criada a tabela do dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -6,10 +6,10 @@ import Header from "../../components/Header";
 import Title from "../../components/Title";
 
 import "./dashboard.css";
-import { FiMessageSquare, FiPlus } from "react-icons/fi";
+import { FiMessageSquare, FiPlus, FiSearch, FiEdit2 } from "react-icons/fi";
 
 export default function Dashboard(){
-  const [chamados, setChamados] = useState([]);
+  const [chamados, setChamados] = useState([1]);
 
   const { signOut } = useContext(AuthContext);
 
@@ -38,6 +38,36 @@ export default function Dashboard(){
                   <FiPlus size={24} color="white" />
                   Novo chamado
                 </Link>
+
+                <table>
+                  <thead>
+                    <tr>
+                      <th scope="col">Cadastrado em</th>
+                      <th scope="col">Cliente</th>
+                      <th scope="col">Assunto</th>
+                      <th scope="col">Status</th>
+                      <th scope="col">#</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td data-label="Cadastrado em">20/11/2021</td>
+                      <td data-label="Cliente">Dev Master</td>
+                      <td data-label="Assunto">Suporte</td>
+                      <td data-label="Status">
+                        <span className="badge" style={{backgroundColor: '#5cb85c'}}>Em aberto</span>
+                      </td>
+                      <td data-label="#">
+                        <button className="action" style={{backgroundColor: "#3583f6"}}>
+                          <FiSearch color="white" />
+                        </button>
+                        <button className="action" style={{backgroundColor: "#F6a935"}}>
+                          <FiEdit2 color="white" />
+                        </button>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
               </>
             )}
         </div>
