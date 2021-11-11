ca404451909140b0d538d562007e8f28f0e5c75a
Wesley <wesley.brazil@outlook.com>
2021-11-11 15:54:42 -0300

    Dev: Página de novo chamado recebendo dados do bd

--- src/services/firebaseConnection.js
+++ src/services/firebaseConnection.js
@@ -2,7 +2,6 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
 import "firebase/storage";
-// import "dotenv/config";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
